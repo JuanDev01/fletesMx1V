c16ac749ef1dbd22b0486e3fdf052c58d4dbe47d
Juan Canez <113712035+JuanDev01@users.noreply.github.com>
2024-05-19 14:19:32 -0700

    1st Draft Of fletesMx

---
+++ models/Post.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  cloudinaryId: {
+    type: String,
+    required: true,
+  },
+  caption: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  contactInfo: {
+    type: mongoose.Schema.Types.Mixed,
+    required: true,
+  },
+  description: {
+    type: mongoose.Schema.Types.Mixed,
+    required: true,
+  },
+  numberOfUnits: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+module.exports = mongoose.model("Post", PostSchema);
